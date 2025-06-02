53427d8876269e6f99f95b8616239e449ba67f9d
Isaac Lovy <isaaclovy@gmail.com>
2025-06-02 12:20:01 -0400

    Sets influences and names bones
    Also correctly getting children past the first included in chain

--- Blender Rigging/IKFK_switch.py
+++ Blender Rigging/IKFK_switch.py
@@ -10,12 +10,14 @@ def add_ik_constraint (target_name, bone, pole_name):
     constraint.chain_count = 2
     constraint.pole_angle = 180
 
-def add_copy_transform(bone_name, target_name):
+def add_copy_transform(bone_name, target_name, constraint_name, influence):
     armature = bpy.context.view_layer.objects.active
     bone = armature.pose.bones[bone_name]
     constraint = bone.constraints.new('COPY_TRANSFORMS')
     constraint.target = armature
     constraint.subtarget = target_name
+    constraint.influence = influence
+    constraint.name = constraint_name
 
 def copy_rename_bones(armature, bone_chain, prefix):
     root_bone_name = prefix + bone_chain[0].name
@@ -33,14 +35,18 @@ def copy_rename_bones(armature, bone_chain, prefix):
     return copied_bones
     
 
-def copy_bones(num_links):
+def main():
     bpy.ops.object.mode_set(mode='EDIT')
     armature = bpy.context.view_layer.objects.active
-    root_bone = bpy.context.selected_editable_bones[0]
+    base_bone = bpy.context.selected_editable_bones[0]
     bone_chain = []
-    bone_chain.append(root_bone)
-    for i in range(num_links - 1):
-        bone_chain.append(root_bone.children[i])
+    bone_chain.append(base_bone)
+    current = base_bone
+
+    # 2 gets shoulder, elbow, and wrist
+    for i in range(2):
+        current = current.children[0]
+        bone_chain.append(current)
 
     fk_bones = copy_rename_bones(armature, bone_chain, "FK_")
     ik_bones = copy_rename_bones(armature, bone_chain, "IK_")
@@ -48,8 +54,11 @@ def copy_bones(num_links):
     bpy.ops.object.mode_set(mode='POSE')
 
     for bone in bone_chain:
-        add_copy_transform(bone.name, "FK_" + bone.name)
-        add_copy_transform(bone.name, "IK_" + bone.name)
+        add_copy_transform(bone.name, 'FK_' + bone.name, 'FK_COPY', 1)
+        add_copy_transform(bone.name, 'IK_' + bone.name, 'IK_COPY', 0)
+    
+    root_bone = bpy.context.object.pose.bones['ROOT']
+    root_bone['IK FK'] = 1.0
 
 
-copy_bones(2)
+main()
