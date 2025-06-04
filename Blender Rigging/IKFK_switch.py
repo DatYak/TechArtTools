adf37a9ba5f157e7e385155c5a1ceaed888f2557
Isaac Lovy <isaaclovy@gmail.com>
2025-06-04 14:20:06 -0400

    Switching via Driver functional
    Still need to add the IK constraints

--- Blender Rigging/IKFK_switch.py
+++ Blender Rigging/IKFK_switch.py
@@ -10,14 +10,14 @@ def add_ik_constraint (target_name, bone, pole_name):
     constraint.chain_count = 2
     constraint.pole_angle = 180
 
-def add_copy_transform(bone_name, target_name, constraint_name, influence):
+def add_copy_transform(bone_name, prefix, target_name, influence):
     armature = bpy.context.view_layer.objects.active
     bone = armature.pose.bones[bone_name]
     constraint = bone.constraints.new('COPY_TRANSFORMS')
     constraint.target = armature
-    constraint.subtarget = target_name
+    constraint.subtarget = prefix + '_' + target_name
     constraint.influence = influence
-    constraint.name = constraint_name
+    constraint.name = prefix + '_COPY'
 
 def copy_rename_bones(armature, bone_chain, prefix):
     root_bone_name = prefix + bone_chain[0].name
@@ -32,33 +32,46 @@ def copy_rename_bones(armature, bone_chain, prefix):
             parent = copy_bone.name
         copied_bones.append(copy_bone)
 
-    return copied_bones
-    
-
 def main():
     bpy.ops.object.mode_set(mode='EDIT')
     armature = bpy.context.view_layer.objects.active
     base_bone = bpy.context.selected_editable_bones[0]
     bone_chain = []
+
+    # Add three bones in total: Shoulder, Elbow, and Wrist
     bone_chain.append(base_bone)
     current = base_bone
-
-    # 2 gets shoulder, elbow, and wrist
     for i in range(2):
         current = current.children[0]
         bone_chain.append(current)
 
-    fk_bones = copy_rename_bones(armature, bone_chain, "FK_")
-    ik_bones = copy_rename_bones(armature, bone_chain, "IK_")
+    copy_rename_bones(armature, bone_chain, "FK_")
+    copy_rename_bones(armature, bone_chain, "IK_")
 
     bpy.ops.object.mode_set(mode='POSE')
 
     for bone in bone_chain:
-        add_copy_transform(bone.name, 'FK_' + bone.name, 'FK_COPY', 1)
-        add_copy_transform(bone.name, 'IK_' + bone.name, 'IK_COPY', 0)
+        add_copy_transform(bone.name, 'FK', bone.name, 1)
+        add_copy_transform(bone.name, 'IK', bone.name, 0)
     
     root_bone = bpy.context.object.pose.bones['ROOT']
-    root_bone['IK FK'] = 1.0
+    root_bone['IKFK_Switch'] = 1.0
 
+    for edit_bone in bone_chain:
+        bone = bpy.context.object.pose.bones[edit_bone.name]
+        ik_curve = bone.constraints['IK_COPY'].driver_add('influence')
+        ik_driver = ik_curve.driver
+        fk_curve = bone.constraints['FK_COPY']
+
+        ik_driver.type = 'SCRIPTED'
+        ik_driver.expression = 'IKFK_Switch'
+
+        v = ik_driver.variables.new()
+        v.name = 'IKFK_Switch'
+
+        t = v.targets[0]
+        t.id_type = 'OBJECT'
+        t.id = bpy.data.objects[armature.name]
+        t.data_path = 'pose.bones["ROOT"]["IKFK_Switch"]'
 
 main()
